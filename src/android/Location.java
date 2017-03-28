1247f853d4a4a7e6062a43e61a0aa5d91e4d5f16
zjhiphop <zjhiphop@gmail.com>
2017-03-28 10:19:42 +0800

    fix sign error and optimize fence flow

--- src/android/Location.java
+++ src/android/Location.java
@@ -1,8 +1,11 @@
 package com.phonegap.plugins.aMapLocation;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
@@ -27,6 +30,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
@@ -45,11 +49,37 @@ public class Location extends CordovaPlugin implements AMapLocationListener {
     //实例化地理围栏客户端
     GeoFenceClient mGeoFenceClient = null;
 
+    public static String sHA1(Context context) {
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(
+                    context.getPackageName(), PackageManager.GET_SIGNATURES);
+            byte[] cert = info.signatures[0].toByteArray();
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            byte[] publicKey = md.digest(cert);
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < publicKey.length; i++) {
+                String appendString = Integer.toHexString(0xFF & publicKey[i])
+                        .toUpperCase(Locale.US);
+                if (appendString.length() == 1)
+                    hexString.append("0");
+                hexString.append(appendString);
+                hexString.append(":");
+            }
+            String result = hexString.toString();
+            return result.substring(0, result.length() - 1);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    public boolean execute(String action, final JSONArray args, CallbackContext callbackContext) throws JSONException {
         callback = callbackContext;
 
-        if(locationClient == null) {
+        if (locationClient == null) {
             locationClient = new AMapLocationClient(this.cordova.getActivity().getApplicationContext());
 //            locationClient.setApiKey("e41317b3e3c5b9227a0795d3e1abd472");
         }
@@ -105,99 +135,15 @@ public class Location extends CordovaPlugin implements AMapLocationListener {
             callback.success();
             return true;
         } else if (action.equals("addCircleRegionForMonitoringWithCenter")) {
-            //实例化地理围栏客户端
-            if (mGeoFenceClient == null) {
-                mGeoFenceClient = new GeoFenceClient(this.cordova.getActivity().getApplicationContext());
-            }
 
-            //设置希望侦测的围栏触发行为，默认只侦测用户进入围栏的行为
-            //public static final int GEOFENCE_IN 进入地理围栏
-            //public static final int GEOFENCE_OUT 退出地理围栏
-            //public static final int GEOFENCE_STAYED 停留在地理围栏内10分钟
-            final long latitude = args.getLong(0);
-            final long longitude = args.getLong(1);
-            final long radius = args.getLong(2);
-            final String customId = args.getString(3);
 
-            mGeoFenceClient.setActivateAction(mGeoFenceClient.GEOFENCE_IN | mGeoFenceClient.GEOFENCE_OUT | mGeoFenceClient.GEOFENCE_STAYED);
-
-            /*
-                mGeoFenceClient.addGeoFence(Point point,float radius, String customId);
-
-                point
-                围栏中心点
-
-                radius
-                要创建的围栏半径 ，半径无限制，单位米
-
-                customId 
-                与围栏关联的自有业务Id
-            */
-
-            //创建一个中心点坐标
-            DPoint centerPoint = new DPoint();
-            //设置中心点纬度
-            centerPoint.setLatitude(latitude);
-            //设置中心点经度
-            centerPoint.setLongitude(longitude);
-            //执行添加围栏的操作
-            mGeoFenceClient.addGeoFence(centerPoint, radius, customId);
-
-            //创建回调监听
-            GeoFenceListener fenceListenter = new GeoFenceListener() {
-
-                public void onGeoFenceCreateFinished(
-                        List<GeoFence> geoFenceList,
-                        int errorCode,
-                        String info
-                ) {
-
-                    Log.d("[GENCE CHANGED]", errorCode + ' ' + info);
-
-                    if (errorCode == GeoFence.ADDGEOFENCE_SUCCESS) {//判断围栏是否创建成功
-                        //tvReult.setText("添加围栏成功!!");
-                        //geoFenceList就是已经添加的围栏列表，可据此查看创建的围栏
-                    } else {
-                        //geoFenceList就是已经添加的围栏列表
-                        //tvReult.setText("添加围栏失败!!");
-                    }
+            this.cordova.getThreadPool().execute(new Runnable() {
+                @Override
+                public void run() {
+                    addCircleRegionForMonitoringWithCenter(args);
                 }
-            };
-            //设置回调监听
-            mGeoFenceClient.setGeoFenceListener(fenceListenter);
+            });
 
-
-            //定义接收广播的action字符串
-            final String GEOFENCE_BROADCAST_ACTION = "com.cloudtea.geofence.broadcast";
-            //创建并设置PendingIntent
-            mGeoFenceClient.createPendingIntent(GEOFENCE_BROADCAST_ACTION);
-
-            BroadcastReceiver mGeoFenceReceiver = new BroadcastReceiver() {
-
-
-                public void onReceive(Context context, Intent intent) {
-                    if (intent.getAction().equals(GEOFENCE_BROADCAST_ACTION)) {
-                        //解析广播内容
-                        //获取Bundle
-                        Bundle bundle = intent.getExtras();
-                        //获取围栏行为：
-                        int status = bundle.getInt(GeoFence.BUNDLE_KEY_FENCESTATUS);
-                        //获取自定义的围栏标识：
-                        String customId = bundle.getString(GeoFence.BUNDLE_KEY_CUSTOMID);
-                        //获取围栏ID:
-                        String fenceId = bundle.getString(GeoFence.BUNDLE_KEY_FENCEID);
-                        //获取当前有触发的围栏对象：
-                        GeoFence fence = bundle.getParcelable(GeoFence.BUNDLE_KEY_FENCE);
-
-                        // todo: 发送处理结果给插件
-                    }
-                }
-            };
-
-
-            IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
-            filter.addAction(GEOFENCE_BROADCAST_ACTION);
-            this.cordova.getActivity().registerReceiver(mGeoFenceReceiver, filter);
             return true;
         } else if (action.equals("removeAllFence")) {
             //实例化地理围栏客户端
@@ -213,6 +159,146 @@ public class Location extends CordovaPlugin implements AMapLocationListener {
         }
     }
 
+    private void addCircleRegionForMonitoringWithCenter(JSONArray args) {
+        //实例化地理围栏客户端
+        if (mGeoFenceClient == null) {
+            String sha1 = sHA1(this.cordova.getActivity().getApplicationContext());
+            mGeoFenceClient = new GeoFenceClient(this.cordova.getActivity().getApplicationContext());
+            Log.d("sha1", "sha1: " + sha1);
+        }
+
+
+        long latitude = 0;
+        long longitude = 0;
+        long radius = 0;
+        String customId = "";
+
+        //设置希望侦测的围栏触发行为，默认只侦测用户进入围栏的行为
+        //public static final int GEOFENCE_IN 进入地理围栏
+        //public static final int GEOFENCE_OUT 退出地理围栏
+        //public static final int GEOFENCE_STAYED 停留在地理围栏内10分钟
+        try {
+
+            latitude = args.getLong(0);
+            longitude = args.getLong(1);
+            radius = args.getLong(2);
+            customId = args.getString(3);
+
+        } catch (JSONException e) {
+            Log.e("[JSON ERROR]", e.toString());
+        }
+
+        mGeoFenceClient.setActivateAction(mGeoFenceClient.GEOFENCE_IN | mGeoFenceClient.GEOFENCE_OUT | mGeoFenceClient.GEOFENCE_STAYED);
+
+            /*
+                mGeoFenceClient.addGeoFence(Point point,float radius, String customId);
+
+                point
+                围栏中心点
+
+                radius
+                要创建的围栏半径 ，半径无限制，单位米
+
+                customId
+                与围栏关联的自有业务Id
+            */
+
+        //创建一个中心点坐标
+        DPoint centerPoint = new DPoint();
+        //设置中心点纬度
+        centerPoint.setLatitude(latitude);
+        //设置中心点经度
+        centerPoint.setLongitude(longitude);
+
+        //执行添加围栏的操作
+        mGeoFenceClient.addGeoFence(centerPoint, radius, customId);
+
+        //创建回调监听
+        GeoFenceListener fenceListenter = new GeoFenceListener() {
+
+            @Override
+            public void onGeoFenceCreateFinished(
+                    List<GeoFence> geoFenceList,
+                    int errorCode,
+                    String customId
+            ) {
+
+                Log.d("[FENCE]", "error code：" + errorCode + "， customID：" + customId);
+
+                if (errorCode == GeoFence.ADDGEOFENCE_SUCCESS) {//判断围栏是否创建成功
+                    //tvReult.setText("添加围栏成功!!");
+                    //geoFenceList就是已经添加的围栏列表，可据此查看创建的围栏
+                    for (GeoFence item : geoFenceList) {
+                        Log.d("[FENCE]", item.getCenter().toString());
+                    }
+
+                    PluginResult result = new PluginResult(PluginResult.Status.OK, "地理围栏创建成功！");
+                    callback.sendPluginResult(result);
+                } else {
+                    //geoFenceList就是已经添加的围栏列表
+                    //tvReult.setText("添加围栏失败!!");
+                    PluginResult result = new PluginResult(PluginResult.Status.ERROR, "地理围栏创建失败：！" + errorCode);
+                    callback.sendPluginResult(result);
+                }
+            }
+        };
+        //设置回调监听
+        mGeoFenceClient.setGeoFenceListener(fenceListenter);
+
+
+        //定义接收广播的action字符串
+        final String GEOFENCE_BROADCAST_ACTION = "com.cloudtea.geofence.broadcast";
+        //创建并设置PendingIntent
+        mGeoFenceClient.createPendingIntent(GEOFENCE_BROADCAST_ACTION);
+
+        BroadcastReceiver mGeoFenceReceiver = new BroadcastReceiver() {
+
+
+            public void onReceive(Context context, Intent intent) {
+                if (intent.getAction().equals(GEOFENCE_BROADCAST_ACTION)) {
+                    fenceReceived(context, intent);
+                }
+            }
+        };
+
+
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        filter.addAction(GEOFENCE_BROADCAST_ACTION);
+        this.cordova.getActivity().registerReceiver(mGeoFenceReceiver, filter);
+    }
+
+    private void fenceReceived(Context context, Intent intent) {
+        if (mGeoFenceClient != null) {
+
+            JSONObject fenceInfo = new JSONObject();
+            //解析广播内容
+            //获取Bundle
+            Bundle bundle = intent.getExtras();
+            //获取围栏行为：
+            int status = bundle.getInt(GeoFence.BUNDLE_KEY_FENCESTATUS);
+            //获取自定义的围栏标识：
+            String customId = bundle.getString(GeoFence.BUNDLE_KEY_CUSTOMID);
+            //获取围栏ID:
+            String fenceId = bundle.getString(GeoFence.BUNDLE_KEY_FENCEID);
+            //获取当前有触发的围栏对象：
+            GeoFence fence = bundle.getParcelable(GeoFence.BUNDLE_KEY_FENCE);
+
+            // todo: 发送处理结果给插件
+            try {
+
+                fenceInfo.put("status", status);
+                fenceInfo.put("customeId", customId);
+                fenceInfo.put("fenceId", fenceId);
+                fenceInfo.put("fence", fence);
+
+            } catch (JSONException e) {
+                Log.e(TAG, "fence json error:" + e);
+            }
+
+            PluginResult result = new PluginResult(PluginResult.Status.OK, fenceInfo);
+            callback.sendPluginResult(result);
+        }
+    }
 
     @Override
     public void onLocationChanged(AMapLocation aMapLocation) {
